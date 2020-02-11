e8247e00062996c83c62305bdf1573975d488998
thebantz <nicoledbanta@gmail.com>
2020-02-10 21:37:57 -0800

    Accomplish initial steps

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,13 +22,20 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+
+      console.log('callCount is ', blinkyDancer.step.callCount);
+
       clock.tick(timeBetweenSteps);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+
+
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
-
       clock.tick(timeBetweenSteps);
+
+      console.log('callCount is ', blinkyDancer.step.callCount);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
+      clock.tick(timeBetweenSteps);
     });
   });
 });
